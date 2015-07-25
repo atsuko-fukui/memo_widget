c39133aa8a33f0f4a5f5d2f310b5d6ec24a32a52
arthurvc <arthurvc0120@gmail.com>
2015-07-26 04:28:39 +0900

    Add image resoureces

--- Me/app/src/main/java/com/reopa/kikuna/memowidget/MemosListFragment.java
+++ Me/app/src/main/java/com/reopa/kikuna/memowidget/MemosListFragment.java
@@ -2,13 +2,18 @@ package com.reopa.kikuna.memowidget;
 
 import android.app.AlertDialog;
 import android.app.ListFragment;
+import android.content.ClipData;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.reopa.kikuna.memowidget.entity.MemosEntity;
+import com.reopa.kikuna.memowidget.adapter.MemoListAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,50 +23,42 @@ import java.util.List;
  */
 public class MemosListFragment extends ListFragment {
 
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
-    updateList();
-  }
-
-  public void updateList() {
-    final List<MemosEntity> entityList = MemosUtils.getLatestMemosFromDb(getActivity());
-    if (entityList.size() == 0) {
-      return;
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        updateList();
     }
 
-    List<String>  memoTexts = new ArrayList<>();
-    for (MemosEntity entity : entityList) {
-      memoTexts.add(entity.getMemoText());
+    public void updateList() {
+        final List<MemosEntity> entityList = MemosUtils.getLatestMemosFromDb(getActivity());
+
+        MemoListAdapter adapter = new MemoListAdapter(getActivity(),entityList);
+        setListAdapter(adapter);
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> items, View view, int position, long id) {
+                final int selectedItemId = entityList.get(position).getId();
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setMessage(R.string.confirm_delete);
+                builder.setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                MemosUtils.deleteMemoById(selectedItemId, getActivity());
+                                updateList();
+                            }
+                        });
+                builder.setNegativeButton(android.R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                builder.create().show();
+            }
+        });
+        MyWidgetProvider.updateWidget(getActivity());
     }
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-            android.R.layout.simple_list_item_1,
-            memoTexts);
-    setListAdapter(adapter);
-    getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
-      @Override
-      public void onItemClick(AdapterView<?> items, View view, int position, long id) {
-        final int selectedItemId = entityList.get(position).getId();
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.confirm_delete);
-        builder.setPositiveButton(android.R.string.ok,
-                new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(DialogInterface dialog, int which) {
-                    MemosUtils.deleteMemoById(selectedItemId, getActivity());
-                    updateList();
-                  }
-                });
-        builder.setNegativeButton(android.R.string.cancel,
-                new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                  }
-                });
-        builder.create().show();
-      }
-    });
-    MyWidgetProvider.updateWidget(getActivity());
-  }
 }
---
+++ Me/app/src/main/java/com/reopa/kikuna/memowidget/adapter/MemoListAdapter.java
@@ -0,0 +1,80 @@
+package com.reopa.kikuna.memowidget.adapter;
+
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.reopa.kikuna.memowidget.R;
+import com.reopa.kikuna.memowidget.entity.MemosEntity;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by arthurvc on 2015/07/22.
+ */
+public class MemoListAdapter extends ArrayAdapter<MemosEntity> {
+
+    /**
+     * Memolist layout resource ID.
+     */
+    private static final int ID_RESOURCE = R.layout.memos_list_fragment_layout;
+    /**
+     * Textview ID.
+     */
+    private static final int ID_LAYOUT_MEMOTEXT = R.id.memo_text;
+    /**
+     * Textview background ID list.
+     */
+    private static final int ID_DRAWABLE_MEMO_BG[] =
+            {R.drawable.husen_0, R.drawable.husen_1, R.drawable.husen_2, R.drawable.husen_3};
+
+    private LayoutInflater mInflater;
+    private List<MemosEntity> mListItems;
+
+
+    public MemoListAdapter(Context context, List<MemosEntity> items) {
+        super(context, ID_RESOURCE, items);
+
+        this.mListItems = items;
+        this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Get contentView by arg.
+        View view = convertView;
+
+        // If contentView is null, create new View by LayoutInflater.
+        if (view == null) {
+            view = mInflater.inflate(ID_RESOURCE, null);
+        }
+
+        if(mListItems.size() == 0){
+            return view;
+        }
+
+        // Get MemoEntity object that match the position.
+        MemosEntity memo = mListItems.get(position);
+
+        // Set memotext.
+        TextView memoTextView = (TextView) view.findViewById(ID_LAYOUT_MEMOTEXT);
+        setMemoText(memoTextView, memo.getMemoText());
+
+        return view;
+    }
+
+    private void setMemoText(TextView view, String text) {
+        // Set memo text.
+        view.setText(text);
+
+        // Create a randam number to select a background image.
+        Random r = new Random();
+        int num = r.nextInt(ID_DRAWABLE_MEMO_BG.length);
+        view.setBackgroundResource(ID_DRAWABLE_MEMO_BG[num]);
+    }
+}
